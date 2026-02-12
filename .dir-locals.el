a321c813755a37936d01c429115978ba719710c7
Aryadev Chavali <aryadev@aryadevchavali.com>
2026-02-12 01:04:01 +0000

    Hello, world!

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((compile-command . "make MODE=debug run")
+         (+license/license-choice . "Unlicense")))
+ (c++-mode . ((mode . clang-format))))
---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+/* main.cpp: Entrypoint
+ * Created: 2026-02-12
+ * Author: Aryadev Chavali
+ * License: See end of file
+ * Commentary:
+ */
+
+#include <cstdio>
+
+int main(void)
+{
+  puts("Hello, World!");
+  return 0;
+}
+
+/* Copyright (C) 2026 Aryadev Chavali
+
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the Unlicense for details.
+
+ * You may distribute and modify this code under the terms of the Unlicense,
+ * which you should have received a copy of along with this program.  If not,
+ * please go to <https://unlicense.org/>.
+
+ */
