e2b002d739eec34ba0556306e25429c6353bdf3d
Aryadev Chavali <aryadev@aryadevchavali.com>
2026-02-13 05:30:39 +0000

    fixed some bugs with wrong neighbour selection in random strategy
    I've gone for a start pointer approach on the array instead of a size
    pointer - realised the algorithms implemented for the reduction
    preprocesser definitions (ACTORS_REPLACE_CELLS,
    ACTORS_BACKWARD_MOVEMENT) hadn't validated the tail end (so a size
    based algorithm wouldn't work).  This seems to have fixed most of the
    issues.
    
    Also made GRID a square (only one dimension required).

--- main.cpp
+++ main.cpp
@@ -16,6 +16,7 @@
 #include <raylib.h>
 #include <raymath.h>
 #include <tuple>
+#include <utility>
 #include <vector>
 
 #include "./prick_aliases.h"
@@ -25,22 +26,26 @@ using opt = std::optional<T>;
 using std::pair;
 using std::tuple;
 
-constexpr u64 WIDTH       = 1024;
-constexpr u64 HEIGHT      = 1024;
-constexpr u64 GRID_WIDTH  = 128;
-constexpr u64 GRID_HEIGHT = 128;
-constexpr u64 NUM_ACTORS  = 100;
-constexpr f32 DELTA_TIME  = 100;
+constexpr u64 WIDTH      = 1024;
+constexpr u64 HEIGHT     = 1024;
+constexpr u64 GRID_SIZE  = 64;
+constexpr u64 NUM_ACTORS = 64;
+constexpr f32 DELTA_TIME = 1;
 
 // should actors be allowed to go back on themselves?
 #define ACTORS_BACKWARD_MOVEMENT 1
 // should actors be able to replace other cells?
 #define ACTORS_REPLACE_CELLS 0
 
+struct Coord
+{
+  u64 x, y;
+};
+
 struct Actor
 {
   u32 id;
-  u64 x, y;
+  Coord pos, target;
 };
 
 struct Grid
@@ -50,10 +55,10 @@ struct Grid
 
   Grid()
   {
-    cells = new u32[GRID_WIDTH * GRID_HEIGHT];
+    cells = new u32[GRID_SIZE * GRID_SIZE];
     // NOTE: Debug builds don't seem to get cleared cells of memory from heap
     // memory.  I thought C++ did this by default?  That's wild.
-    memset(cells, 0, GRID_WIDTH * GRID_HEIGHT * sizeof(cells[0]));
+    memset(cells, 0, GRID_SIZE * GRID_SIZE * sizeof(cells[0]));
   }
 
   ~Grid(void)
@@ -63,7 +68,12 @@ struct Grid
 
   u32 &operator[](u64 x, u64 y)
   {
-    return cells[(x * GRID_WIDTH) + y];
+    return cells[(x * GRID_SIZE) + y];
+  }
+
+  u32 &operator[](Coord p)
+  {
+    return cells[(p.x * GRID_SIZE) + p.y];
   }
 
   tuple<u64, u64, u64> get_cell_dimensions(void)
@@ -76,9 +86,9 @@ struct Grid
 
     // we take the smaller of the two pixels per cell as our overall size.
     u64 cell_dimension =
-        std::min((WIDTH / 1.5) / GRID_WIDTH, (HEIGHT / 1.5) / GRID_HEIGHT);
-    u64 padding_x = (WIDTH - (cell_dimension * GRID_WIDTH)) / 2;
-    u64 padding_y = (HEIGHT - (cell_dimension * GRID_HEIGHT)) / 2;
+        std::min((WIDTH / 1.5) / GRID_SIZE, (HEIGHT / 1.5) / GRID_SIZE);
+    u64 padding_x = (WIDTH - (cell_dimension * GRID_SIZE)) / 2;
+    u64 padding_y = (HEIGHT - (cell_dimension * GRID_SIZE)) / 2;
 
     return {cell_dimension, padding_x, padding_y};
   }
@@ -87,13 +97,13 @@ struct Grid
   {
     auto [cell_dimension, padding_x, padding_y] = get_cell_dimensions();
 
-    for (u64 x = 0; x < GRID_WIDTH; ++x)
+    for (u64 x = 0; x < GRID_SIZE; ++x)
     {
-      for (u64 y = 0; y < GRID_HEIGHT; ++y)
+      for (u64 y = 0; y < GRID_SIZE; ++y)
       {
         u64 cell_x      = (x * cell_dimension) + padding_x;
         u64 cell_y      = (y * cell_dimension) + padding_y;
-        u32 cell_colour = cells[(x * GRID_WIDTH) + y];
+        u32 cell_colour = (*this)[x, y];
         Color col       = {};
         memcpy(&col, &cell_colour, sizeof(Color));
 
@@ -105,72 +115,74 @@ struct Grid
 
     for (const auto &actor : actors)
     {
-      u64 x = (actor.x * cell_dimension) + padding_x;
-      u64 y = (actor.y * cell_dimension) + padding_y;
+      u64 x = (actor.pos.x * cell_dimension) + padding_x;
+      u64 y = (actor.pos.y * cell_dimension) + padding_y;
       DrawRectangleLines(x, y, cell_dimension, cell_dimension, BLUE);
     }
   }
 
-  void add_actor(u32 id, u64 x, u64 y)
+  void add_actor(u32 id, Coord pos, Coord target)
   {
-    actors.push_back({id, x, y});
-    cells[(x * GRID_WIDTH) + y] = id;
+    for (const auto &actor : actors)
+    {
+      assert(actor.id != id);
+    }
+    actors.push_back({id, pos, target});
+    cells[(pos.x * GRID_SIZE) + pos.y] = id;
   }
 
-  u64 get_neighbours(u64 x, u64 y, std::array<tuple<u64, u64, u32>, 4> &arr)
+  u64 get_neighbours(u64 x, u64 y, std::array<pair<Coord, u32>, 4> &arr)
   {
-    assert(x < GRID_WIDTH && y < GRID_HEIGHT);
+    assert(x < GRID_SIZE && y < GRID_SIZE);
     u64 size = 0;
     if (x > 0)
-      arr[size++] = {x - 1, y, cells[((x - 1) * GRID_WIDTH) + y]};
-    if (x < GRID_WIDTH - 1)
-      arr[size++] = {x + 1, y, cells[((x + 1) * GRID_WIDTH) + y]};
+      arr[size++] = {{x - 1, y}, cells[((x - 1) * GRID_SIZE) + y]};
+    if (x < GRID_SIZE - 1)
+      arr[size++] = {{x + 1, y}, cells[((x + 1) * GRID_SIZE) + y]};
     if (y > 0)
-      arr[size++] = {x, y - 1, cells[(x * GRID_WIDTH) + (y - 1)]};
-    if (y < GRID_HEIGHT - 1)
-      arr[size++] = {x, y + 1, cells[(x * GRID_WIDTH) + (y + 1)]};
+      arr[size++] = {{x, y - 1}, cells[(x * GRID_SIZE) + (y - 1)]};
+    if (y < GRID_SIZE - 1)
+      arr[size++] = {{x, y + 1}, cells[(x * GRID_SIZE) + (y + 1)]};
     return size;
   }
 
   // random strategy: pick a random cell
   void actor_random_strat(Actor &actor)
   {
-    std::array<tuple<u64, u64, u32>, 4> neighbours;
-    u64 size = get_neighbours(actor.x, actor.y, neighbours);
+    std::array<pair<Coord, u32>, 4> neighbours;
+    u64 size  = get_neighbours(actor.pos.x, actor.pos.y, neighbours);
+    u64 start = 0;
 #if !ACTORS_BACKWARD_MOVEMENT
-    for (u64 i = 0; i < size; ++i)
+    for (u64 i = start; i < size; ++i)
     {
-      if (std::get<2>(neighbours[i]) == actor.id)
+      if (neighbours[i].second == actor.id)
       {
-        --size;
-        auto x           = neighbours[size];
-        neighbours[size] = neighbours[i];
-        neighbours[i]    = x;
+        std::swap(neighbours[start], neighbours[i]);
+        ++start;
       }
     }
 #endif
 #if !ACTORS_REPLACE_CELLS
-    for (u64 i = 0; i < size; ++i)
+    for (u64 i = start; i < size; ++i)
     {
-      if (!(std::get<2>(neighbours[i]) == actor.id ||
-            std::get<2>(neighbours[i]) == 0))
+      if (!(neighbours[i].second == actor.id || neighbours[i].second == 0))
       {
-        --size;
-        auto x           = neighbours[size];
-        neighbours[size] = neighbours[i];
-        neighbours[i]    = x;
+        std::swap(neighbours[start], neighbours[i]);
+        ++start;
       }
     }
 #endif
-
-    if (size == 0)
+    if (start == size)
       return;
-    u64 ind                     = rand() % size;
-    auto [x, y, cell]           = neighbours[ind];
-    cells[(x * GRID_WIDTH) + y] = actor.id;
-    actor.x                     = x;
-    actor.y                     = y;
+
+    u64 ind            = rand() % (size - start);
+    auto [coord, cell] = neighbours[ind + start];
+
+    (*this)[coord] = actor.id;
+    actor.pos      = coord;
   }
+
+  void actor_target_strategy(Actor &actor);
 
   void update(void)
   {
@@ -189,8 +201,8 @@ int main(void)
 
   for (u64 i = 0; i < NUM_ACTORS; ++i)
   {
-    u64 x   = rand() % GRID_WIDTH;
-    u64 y   = rand() % GRID_HEIGHT;
+    u64 x   = rand() % GRID_SIZE;
+    u64 y   = rand() % GRID_SIZE;
     Color c = {
         .r = (u8)(rand() % 200 + 55),
         .g = (u8)(rand() % 200 + 55),
@@ -201,16 +213,27 @@ int main(void)
     u32 packed = 0;
     memcpy(&packed, &c, sizeof(c));
 
-    grid.add_actor(packed, x, y);
+    grid.add_actor(packed, {x, y}, {0, 0});
   }
 
-  f32 timer = 0;
+  bool paused = true;
+  f32 timer   = 0;
+  (void)timer;
   InitWindow(WIDTH, HEIGHT, "Random Paths");
   SetTargetFPS(60);
   while (!WindowShouldClose())
   {
+    if (IsKeyPressed(KEY_SPACE))
+    {
+      paused = !paused;
+    }
+    if (IsKeyPressed(KEY_PERIOD))
+    {
+      paused = true;
+      grid.update();
+    }
     timer += 1 / GetFrameTime();
-    if (timer > DELTA_TIME)
+    if (timer > DELTA_TIME && !paused)
     {
       grid.update();
       timer = 0;
